63b59aeff01c7b0df2adcc7909a121de94fff89f
Andrew Erickson <aerickson14@gmail.com>
2014-09-21 09:31:13 -0500

    Initial Commit

---
+++ OCRExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCRExample
+//
+//  Created by Andrew Erickson on 2014-09-21.
+//  Copyright (c) 2014 Andrew Erickson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCRExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCRExample
+//
+//  Created by Andrew Erickson on 2014-09-21.
+//  Copyright (c) 2014 Andrew Erickson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
